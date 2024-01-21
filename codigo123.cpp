41463c583e5c7e6665722e15b94604d883ad0099
joelsebas27III <joelsebastianmamanilopez123@gmail.com>
2024-01-21 18:35:16 -0500

    cuarta version 4 codigos mendez

--- codigo123.cpp
+++ codigo123.cpp
@@ -6,5 +6,9 @@ int main(){
     cout<<"hola mundo"<<endl;
     cout<<"hola mundo"<<endl;
     cout<<"hola mundo"<<endl;
+    int n=4;
+    for(int i=0;i<n;i++){
+        cout<<"codigomendez "<<i<<endl;
+    }
     return 0;
 }
